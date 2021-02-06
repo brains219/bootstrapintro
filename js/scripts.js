d882d48f25f8af87597acf05bdfefcaf8cf9c5b5
Brian Angulo <brianangulo@brianMac.home>
2021-02-06 13:29:06 -0500

    Modals are now functioning via jQuery instead of data- attributes

--- js/scripts.js
+++ js/scripts.js
@@ -12,4 +12,15 @@
                         $("#carouselButton").children("i").addClass("fa-pause");
                     }
             });
-        });
+ });
+
+ 
+$("#reserveButton").click(function () {
+    $("#reserveModal").modal("show");
+    
+});
+
+$("#loginButton").click(function () {
+    $("#loginModal").modal("show");
+
+});
